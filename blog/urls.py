38557369681a26d4ee42396ad8875b7f849d3840
deepanker <srivastava.deepanker@gmail.com>
2018-07-20 16:40:32 +0530

    Django Upgrade, detail page added

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
 from . import views
+from django.urls import path
+
 
 urlpatterns = [
-    url('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
