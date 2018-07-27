5b14e22f1494cdc4e8d0ebdf867db91be720472a
deepanker <srivastava.deepanker@gmail.com>
2018-07-27 20:56:43 +0530

    DB Changed to MySQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,9 +75,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/Volumes/Data/Study/dj/mysite/mysql.cnf',
+        },
+#        'NAME': 'polls_db',
+#        'USER': 'polls',
+#        'PASSWORD': 'polls',
+#        'HOST': 'localhost',
+#        'PORT': '',
     }
 }
 
